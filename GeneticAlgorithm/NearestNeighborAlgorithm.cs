02e17a3697269f5c9446beb23651f4280a344ae8
RomanA <126502093+RomanAntoshin@users.noreply.github.com>
2024-05-28 23:18:43 +0300

    Update NearestNeighborAlgorithm.cs

--- GeneticAlgorithm/NearestNeighborAlgorithm.cs
+++ GeneticAlgorithm/NearestNeighborAlgorithm.cs
@@ -8,20 +8,14 @@ namespace GeneticAlgorithm
 {
     class NearestNeighborAlgorithm: Algorithm
     {
-        //private int town;
-        //private Random rnd;
         public NearestNeighborAlgorithm(double[,] M, int town = 0): base(M, town)
         {
-            //town = GenerateStartCity();
-            //rnd = new Random();
         }
         public override Person GiveSolution()
         {
             int ind = 0;
             double min = M[0, 0];
-            //int town = GenerateStartCity();
             Ham.Add(town);
-            //for(int j=0; j<Math.Sqrt(M.Length)-1; j++)
             while (Ham.Count < Math.Sqrt(M.Length))
             {
                 for (int i = 0; i < Math.Sqrt(M.Length); i++)
@@ -34,7 +28,6 @@ namespace GeneticAlgorithm
                 town = ind;
                 min = M[0, 0];
             }
-            //Ham.Add(0);
             return new Person(Ham, GetCriteria());
         }
     }
